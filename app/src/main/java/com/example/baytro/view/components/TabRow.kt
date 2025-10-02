f79d51e629446410357b7a2dcbc5a9565fb82f21
tienloc205 <160756312+tienloc205@users.noreply.github.com>
2025-10-02 19:39:41 -0400

    reinit tabrow component

--- app/src/main/java/com/example/baytro/view/components/TabRow.kt
+++ app/src/main/java/com/example/baytro/view/components/TabRow.kt
@@ -1,15 +1,12 @@
 package com.example.baytro.view.components
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.List
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LeadingIconTab
-import androidx.compose.material3.TabRow
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,10 +15,9 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
-
 @Composable
 fun TabRowComponent(
-    tabItemList : List<Pair<String, ImageVector>>,
+    tabItemList: List<Pair<String, ImageVector>>,
     selectedTabIndex: Int,
     onTabSelected: (Int) -> Unit
 ) {
@@ -29,22 +25,27 @@ fun TabRowComponent(
         Modifier
             .fillMaxWidth()
             .height(50.dp)
-            //.padding(start = 16.dp)
             .background(Color.White)
     ) {
-        TabRow(
+        SecondaryTabRow(
             selectedTabIndex = selectedTabIndex,
-            Modifier.background(Color.White)
+            modifier = Modifier.background(Color.White)
         ) {
-            tabItemList.forEach() { tabItem ->
+            tabItemList.forEachIndexed { index, tabItem ->
                 LeadingIconTab(
-                    selected = selectedTabIndex == 0,
-                    onClick = {onTabSelected(0)},
-                    text = { Text(text = tabItem.first, maxLines = 2, overflow = TextOverflow.Ellipsis) },
+                    selected = selectedTabIndex == index,
+                    onClick = { onTabSelected(index) },
+                    text = {
+                        Text(
+                            text = tabItem.first,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
                     icon = {
                         Icon(
                             imageVector = tabItem.second,
-                            contentDescription = tabItem.first // Add a valid content description
+                            contentDescription = tabItem.first
                         )
                     }
                 )
