a27ca47e72bdc2640730a26b65abec6f7c2a592e
minh2012 <23021632@vnu.edu.vn>
2025-10-13 11:09:28 +0700

    feat: Refactor Room ViewModels to include additional dependencies for improved functionality

--- app/src/main/java/com/example/baytro/di/AppModule.kt
+++ app/src/main/java/com/example/baytro/di/AppModule.kt
@@ -193,9 +193,9 @@ val authModule = module {
     ) }
     viewModel { RequestListVM(get(), get(), get(), get(), get(), get()) }
     viewModel { (handle: SavedStateHandle) -> AddRoomVM(get(), get(), handle) }
-    viewModel { (handle: SavedStateHandle) -> RoomListVM(get(), get(), handle) }
-    viewModel { (handle: SavedStateHandle) -> RoomDetailsVM(get(), handle) }
-    viewModel { (handle: SavedStateHandle) -> EditRoomVM(get(), handle) }
+    viewModel { (handle: SavedStateHandle) -> RoomListVM(get(), get(), get(),handle) }
+    viewModel { (handle: SavedStateHandle) -> RoomDetailsVM(get(),get(),get(), handle) }
+    viewModel { (handle: SavedStateHandle) -> EditRoomVM(get(),get(), handle) }
     viewModel { EditBuildingVM(androidContext(), get(), get(), get()) }
     viewModel { TenantDashboardVM(get(), get(), get(), get(), get()) }
     viewModel { AddRequestVM(get(), get(), get(), get()) }
